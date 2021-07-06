2025ce4e6bfb4808d5e530edfb101c82c6a76808
qinyanhui <qinyanhui@bjxiaoxiang.com>
2021-07-06 10:50:35 +0800

    common edit

--- library_common/src/main/java/com/xxjy/common/base/BaseActivity.kt
+++ library_common/src/main/java/com/xxjy/common/base/BaseActivity.kt
@@ -1,12 +1,97 @@
 package com.xxjy.common.base
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import com.blankj.utilcode.util.BarUtils
+import com.qmuiteam.qmui.arch.QMUIFragmentActivity
 import com.xxjy.common.R
+import com.xxjy.common.util.toastlib.MyToast
+import per.goweii.anylayer.AnyLayer
+import per.goweii.anylayer.Layer.AnimatorCreator
+import per.goweii.anylayer.dialog.DialogLayer
+import per.goweii.anylayer.utils.AnimatorHelper
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : QMUIFragmentActivity() {
+
+    private var mLoadingDialog: DialogLayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        initLoadingDialog()
+        BarUtils.setStatusBarLightMode(this, true)
     }
+
+    /**
+     * 设置透明状态栏-默认为深色
+     */
+    fun setTransparentStatusBar() {
+        BarUtils.transparentStatusBar(this)
+        BarUtils.setStatusBarLightMode(this, true)
+    }
+
+    /**
+     * @param view toolbar设置margin
+     *
+     * 设置透明状态栏的margin
+     */
+    fun setTransparentStatusBar(view: View?) =
+        setTransparentStatusBar(view, true)
+
+    /**
+     * @param isDark 默认为深色
+     *
+     * 设置透明状态栏的颜色
+     */
+    open fun setTransparentStatusBar(view: View?, isDark: Boolean) {
+        BarUtils.transparentStatusBar(this)
+        if (view != null) {
+            BarUtils.addMarginTopEqualStatusBarHeight(view)
+        }
+        BarUtils.setStatusBarLightMode(this, isDark)
+    }
+
+    open fun showToast(msg: String?) =
+        MyToast.showContentToast(this, msg)
+
+    open fun showToastInfo(msg: String?) =
+        MyToast.showInfo(this, msg)
+
+    open fun showToastWarning(msg: String?) =
+        MyToast.showWarning(this, msg)
+
+    open fun showToastError(msg: String?) =
+        MyToast.showError(this, msg)
+
+    open fun showToastSuccess(msg: String?) =
+        MyToast.showSuccess(this, msg)
+
+    private fun initLoadingDialog() {
+        mLoadingDialog = AnyLayer.dialog()
+            .contentView(R.layout.loading_layout)
+            .cancelableOnClickKeyBack(true)
+            .gravity(Gravity.CENTER)
+            .contentAnimator(object : AnimatorCreator {
+                override fun createInAnimator(target: View): Animator? {
+                    return AnimatorHelper.createAlphaInAnim(target)
+                }
+
+                override fun createOutAnimator(target: View): Animator? {
+                    return AnimatorHelper.createAlphaOutAnim(target)
+                }
+            })
+            .cancelableOnTouchOutside(false)
+    }
+
+    /**
+     * 展示读取的 dialog
+     */
+    open fun showLoadingDialog() = mLoadingDialog?.show()
+
+    /**
+     * 隐藏读取的 dialog
+     */
+    open fun dismissLoadingDialog() = mLoadingDialog?.dismiss()
+
 }
---
+++ library_common/src/main/java/com/xxjy/common/util/toastlib/Toasty.java
@@ -0,0 +1,327 @@
+package com.xxjy.common.util.toastlib;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.CheckResult;
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+
+import com.xxjy.common.R;
+
+
+/**
+ * This file is part of Toasty.
+ * <p>
+ * Toasty is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * Toasty is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with Toasty.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+@SuppressLint("InflateParams")
+public class Toasty {
+    @ColorInt
+    private static int DEFAULT_TEXT_COLOR = Color.parseColor("#FFFFFF");
+    @ColorInt
+    private static int ERROR_COLOR = Color.parseColor("#D50000");
+    @ColorInt
+    private static int INFO_COLOR = Color.parseColor("#3F51B5");
+    @ColorInt
+    private static int SUCCESS_COLOR = Color.parseColor("#388E3C");
+    @ColorInt
+    private static int WARNING_COLOR = Color.parseColor("#FFA900");
+    @ColorInt
+    private static int NORMAL_COLOR = Color.parseColor("#353A3E");
+
+    private static final Typeface LOADED_TOAST_TYPEFACE = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
+    private static Typeface currentTypeface = LOADED_TOAST_TYPEFACE;
+    private static int textSize = 16; // in SP
+
+    private static boolean tintIcon = true;
+
+    private static Toast currentToast;  //全局维护一个toast 即可
+
+    private static View toastLayout;    //全局维护一个toastLayout 即可
+    private static ImageView toastIcon;
+    private static TextView toastTextView;
+
+    private Toasty() {
+    }
+
+    @CheckResult
+    public static Toast normal(@NonNull Context context, @NonNull CharSequence message) {
+        return normal(context, message, Toast.LENGTH_SHORT, null, false);
+    }
+
+    @CheckResult
+    public static Toast normal(@NonNull Context context, @NonNull CharSequence message, Drawable icon) {
+        return normal(context, message, Toast.LENGTH_SHORT, icon, true);
+    }
+
+    @CheckResult
+    public static Toast normal(@NonNull Context context, @NonNull CharSequence message, int duration) {
+        return normal(context, message, duration, null, false);
+    }
+
+    @CheckResult
+    public static Toast normal(@NonNull Context context, @NonNull CharSequence message, int duration,
+                               Drawable icon) {
+        return normal(context, message, duration, icon, true);
+    }
+
+    @CheckResult
+    public static Toast normal(@NonNull Context context, @NonNull CharSequence message, int duration,
+                               Drawable icon, boolean withIcon) {
+        return custom(context, message, icon, NORMAL_COLOR, duration, withIcon, true);
+    }
+
+    @CheckResult
+    public static Toast warning(@NonNull Context context, @NonNull CharSequence message) {
+        return warning(context, message, Toast.LENGTH_SHORT, true);
+    }
+
+    @CheckResult
+    public static Toast warning(@NonNull Context context, @NonNull CharSequence message, int duration) {
+        return warning(context, message, duration, true);
+    }
+
+    @CheckResult
+    public static Toast warning(@NonNull Context context, @NonNull CharSequence message, int duration, boolean withIcon) {
+        return custom(context, message, ToastyUtils.getDrawable(context, R.drawable.toast_icon_warning),
+                WARNING_COLOR, duration, withIcon, true);
+    }
+
+    @CheckResult
+    public static Toast info(@NonNull Context context, @NonNull CharSequence message) {
+        return info(context, message, Toast.LENGTH_SHORT, true);
+    }
+
+    @CheckResult
+    public static Toast info(@NonNull Context context, @NonNull CharSequence message, int duration) {
+        return info(context, message, duration, true);
+    }
+
+    @CheckResult
+    public static Toast info(@NonNull Context context, @NonNull CharSequence message, int duration, boolean withIcon) {
+        return custom(context, message, ToastyUtils.getDrawable(context, R.drawable.toast_icon_info),
+                INFO_COLOR, duration, withIcon, true);
+    }
+
+    @CheckResult
+    public static Toast success(@NonNull Context context, @NonNull CharSequence message) {
+        return success(context, message, Toast.LENGTH_SHORT, true);
+    }
+
+    @CheckResult
+    public static Toast success(@NonNull Context context, @NonNull CharSequence message, int duration) {
+        return success(context, message, duration, true);
+    }
+
+    @CheckResult
+    public static Toast success(@NonNull Context context, @NonNull CharSequence message, int duration, boolean withIcon) {
+        return custom(context, message, ToastyUtils.getDrawable(context, R.drawable.toast_icon_success),
+                SUCCESS_COLOR, duration, withIcon, true);
+    }
+
+    @CheckResult
+    public static Toast error(@NonNull Context context, @NonNull CharSequence message) {
+        return error(context, message, Toast.LENGTH_SHORT, true);
+    }
+
+    @CheckResult
+    public static Toast error(@NonNull Context context, @NonNull CharSequence message, int duration) {
+        return error(context, message, duration, true);
+    }
+
+    @CheckResult
+    public static Toast error(@NonNull Context context, @NonNull CharSequence message, int duration, boolean withIcon) {
+        return custom(context, message, ToastyUtils.getDrawable(context, R.drawable.toast_icon_error),
+                ERROR_COLOR, duration, withIcon, true);
+    }
+
+    @CheckResult
+    public static Toast custom(@NonNull Context context, @NonNull CharSequence message, Drawable icon,
+                               int duration, boolean withIcon) {
+        return custom(context, message, icon, -1, duration, withIcon, false);
+    }
+
+    @CheckResult
+    public static Toast custom(@NonNull Context context, @NonNull CharSequence message, @DrawableRes int iconRes,
+                               @ColorInt int tintColor, int duration,
+                               boolean withIcon, boolean shouldTint) {
+        return custom(context, message, ToastyUtils.getDrawable(context, iconRes),
+                tintColor, duration, withIcon, shouldTint);
+    }
+
+    /**
+     * 创建一个 居中显示的 toast
+     *
+     * @param context
+     * @param message    需要展示的消息
+     * @param icon       需要展示的drawable图片
+     * @param tintColor  toast的背景颜色
+     * @param duration   展示时长, short , long 两种
+     * @param withIcon   是否展示icon,一般有icon 会展示
+     * @param shouldTint 是否需要自定义背景颜色
+     * @return
+     */
+    @CheckResult
+    public static Toast custom(@NonNull Context context, @NonNull CharSequence message, Drawable icon,
+                               @ColorInt int tintColor, int duration,
+                               boolean withIcon, boolean shouldTint) {
+        // todo 这一步是后期处理加上的, 目的是 不让背景修改颜色 至此: shouldTint 和 tintColor 两个字段全部无效了
+        shouldTint = false;
+
+        if (currentToast == null) {
+            currentToast = new Toast(context);
+        }
+        if (toastLayout == null) {
+            toastLayout = (LayoutInflater.from(context)).inflate(R.layout.toast_layout, null);
+            toastIcon = (ImageView) toastLayout.findViewById(R.id.toast_icon);
+            toastTextView = (TextView) toastLayout.findViewById(R.id.toast_text);
+        }
+        Drawable drawableFrame;
+
+//        if (shouldTint)
+//            drawableFrame = ToastyUtils.tint9PatchDrawableFrame(context, tintColor);
+//        else
+//            drawableFrame = ToastyUtils.getDrawable(context, R.drawable.toast_frame_range);
+//        ToastyUtils.setBackground(toastLayout, drawableFrame);
+
+        if (withIcon) {
+            if (icon == null)
+                throw new IllegalArgumentException("如果设置 withIcon 为 true,则 icon 不可为空");
+            if (tintIcon)   //tintIcon是用来在全局配置是否展示 icon 的.
+                icon = ToastyUtils.tintIcon(icon, DEFAULT_TEXT_COLOR);
+            toastIcon.setVisibility(View.VISIBLE);
+            ToastyUtils.setBackground(toastIcon, icon);
+        } else {
+            toastIcon.setVisibility(View.GONE);
+        }
+        toastTextView.setTextColor(DEFAULT_TEXT_COLOR);
+        toastTextView.setText(message);
+        toastTextView.setTypeface(currentTypeface);
+        toastTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, textSize);
+
+        currentToast.setView(toastLayout);
+        currentToast.setDuration(duration);
+        currentToast.setGravity(Gravity.CENTER, 0, -150);
+        return currentToast;
+    }
+
+    public static class Config {
+        @ColorInt
+        private int DEFAULT_TEXT_COLOR = Toasty.DEFAULT_TEXT_COLOR;
+        @ColorInt
+        private int ERROR_COLOR = Toasty.ERROR_COLOR;
+        @ColorInt
+        private int INFO_COLOR = Toasty.INFO_COLOR;
+        @ColorInt
+        private int SUCCESS_COLOR = Toasty.SUCCESS_COLOR;
+        @ColorInt
+        private int WARNING_COLOR = Toasty.WARNING_COLOR;
+
+        private Typeface typeface = Toasty.currentTypeface;
+        private int textSize = Toasty.textSize;
+
+        private boolean tintIcon = Toasty.tintIcon;
+
+        private Config() {
+        }
+
+        @CheckResult
+        public static Config getInstance() {
+            return new Config();
+        }
+
+        public static void reset() {
+            Toasty.DEFAULT_TEXT_COLOR = Color.parseColor("#FFFFFF");
+            Toasty.ERROR_COLOR = Color.parseColor("#D50000");
+            Toasty.INFO_COLOR = Color.parseColor("#3F51B5");
+            Toasty.SUCCESS_COLOR = Color.parseColor("#388E3C");
+            Toasty.WARNING_COLOR = Color.parseColor("#FFA900");
+            Toasty.currentTypeface = LOADED_TOAST_TYPEFACE;
+            Toasty.textSize = 16;
+            Toasty.tintIcon = true;
+        }
+
+        @CheckResult
+        public Config setTextColor(@ColorInt int textColor) {
+            DEFAULT_TEXT_COLOR = textColor;
+            return this;
+        }
+
+        @CheckResult
+        public Config setErrorColor(@ColorInt int errorColor) {
+            ERROR_COLOR = errorColor;
+            return this;
+        }
+
+        @CheckResult
+        public Config setInfoColor(@ColorInt int infoColor) {
+            INFO_COLOR = infoColor;
+            return this;
+        }
+
+        @CheckResult
+        public Config setSuccessColor(@ColorInt int successColor) {
+            SUCCESS_COLOR = successColor;
+            return this;
+        }
+
+        @CheckResult
+        public Config setWarningColor(@ColorInt int warningColor) {
+            WARNING_COLOR = warningColor;
+            return this;
+        }
+
+        @CheckResult
+        public Config setToastTypeface(@NonNull Typeface typeface) {
+            this.typeface = typeface;
+            return this;
+        }
+
+        @CheckResult
+        public Config setTextSize(int sizeInSp) {
+            this.textSize = sizeInSp;
+            return this;
+        }
+
+        @CheckResult
+        public Config tintIcon(boolean tintIcon) {
+            this.tintIcon = tintIcon;
+            return this;
+        }
+
+        public void apply() {
+            Toasty.DEFAULT_TEXT_COLOR = DEFAULT_TEXT_COLOR;
+            Toasty.ERROR_COLOR = ERROR_COLOR;
+            Toasty.INFO_COLOR = INFO_COLOR;
+            Toasty.SUCCESS_COLOR = SUCCESS_COLOR;
+            Toasty.WARNING_COLOR = WARNING_COLOR;
+            Toasty.currentTypeface = typeface;
+            Toasty.textSize = textSize;
+            Toasty.tintIcon = tintIcon;
+        }
+    }
+}
